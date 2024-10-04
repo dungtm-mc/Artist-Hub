4d88d1da64feb397eaee59741809045855bcd923
bytesculptors <tuannghia200603@gmail.com>
2024-10-04 13:58:37 +0700

    feat:set up db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ArtistModule } from './artist/artist.module'
+import { DBModule } from './db/db.module'
+import { CampaignModule } from './campaign/campaign.module'
+import { FanModule } from './fan/fan.module'
+import { LabelModule } from './label/label.module'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ArtistModule,
+    DBModule,
+    CampaignModule,
+    FanModule,
+    LabelModule,
+    UserModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
