e69e95da246dca5afe318d60b7ca6fdc5056add8
bytesculptors <tuannghia200603@gmail.com>
2024-10-09 11:50:22 +0700

    feat: update field for widget entity

--- backend/src/widget/entities/widget.entity.ts
+++ backend/src/widget/entities/widget.entity.ts
@@ -1,3 +1,4 @@
+import { WidgetType } from '../../enums'
 import { ArtistPageEntity } from '../../artistPage/entities/artistPage.entity'
 import { AbstractEntity } from '../../common/abstract.entity'
 import { Column, Entity, ManyToOne } from 'typeorm'
@@ -7,6 +8,12 @@ export class WidgetEntity extends AbstractEntity {
   @Column()
   name: string
 
+  @Column({ type: 'enum', enum: WidgetType })
+  type: string
+
   @ManyToOne(() => ArtistPageEntity)
   artistPage: ArtistPageEntity
+
+  @Column({ type: 'varchar', length: 100 })
+  datasource: string
 }
