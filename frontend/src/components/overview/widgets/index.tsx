f7609c3802e7c6813d11b6e6caac470644fd49d5
dungtm-mc <dungtm@missingcorner.com>
2024-10-10 10:29:31 +0700

    feat: table widget

--- frontend/src/components/overview/widgets/index.tsx
+++ frontend/src/components/overview/widgets/index.tsx
@@ -1,6 +1,7 @@
 import BarchartWidget from './barchart-widget'
 import CampaignWidget from './campaign-widget'
 import SingleWidget from './single-widget'
+import TableWidget from './table-widget'
 import styles from './widgets.module.css'
 
 const Widgets = () => {
@@ -42,6 +43,42 @@ const Widgets = () => {
     ]
   }
 
+  const testTableWidget = {
+    title: 'Total',
+    data: [
+      {
+        segment: 'Subscribed but no purchased',
+        fans: '90.2K',
+        ctr: 5.1,
+        avgRevenue: '$40.2K'
+      },
+      {
+        segment: 'Merch only customers',
+        fans: '90.2K',
+        ctr: 5.1,
+        avgRevenue: '$40.2K'
+      },
+      {
+        segment: 'Music only customers',
+        fans: '90.2K',
+        ctr: 5.1,
+        avgRevenue: '$40.2K'
+      },
+      {
+        segment: 'Highly engaged',
+        fans: '90.2K',
+        ctr: 5.1,
+        avgRevenue: '$40.2K'
+      },
+      {
+        segment: 'Heavy Spenders',
+        fans: '90.2K',
+        ctr: 5.1,
+        avgRevenue: '$40.2K'
+      }
+    ]
+  }
+
   return (
     <>
       <div className={styles.test}>
@@ -63,6 +100,12 @@ const Widgets = () => {
       </div>
       <div className={styles.test3}>
         <CampaignWidget />
+      </div>
+      <div className={styles.test4}>
+        <TableWidget
+          title={testTableWidget.title}
+          data={testTableWidget.data}
+        />
       </div>
     </>
   )
---
+++ frontend/src/components/overview/widgets/table-widget/index.tsx
@@ -0,0 +1,48 @@
+import { Table } from '@mantine/core'
+import styles from './table-widget.module.css'
+
+interface TableWidgetProps {
+  title: string
+  data: {
+    segment: string
+    fans: string
+    ctr: number
+    avgRevenue: string
+  }[]
+}
+
+const TableWidget = (props: TableWidgetProps) => {
+  const rows = props.data.map((row) => (
+    <Table.Tr>
+      <Table.Td>{row.segment}</Table.Td>
+      <Table.Td>{row.fans}</Table.Td>
+      <Table.Td>{row.ctr}</Table.Td>
+      <Table.Td className={styles.cellRight}>{row.avgRevenue}</Table.Td>
+    </Table.Tr>
+  ))
+
+  return (
+    <div className={styles.widget}>
+      <div className={styles.title}>
+        Segment by <b>{props.title}</b>
+      </div>
+      <div className={styles.table}>
+        <Table horizontalSpacing="lg">
+          <Table.Thead className={styles.headerRow}>
+            <Table.Tr>
+              <Table.Th className={styles.header}>Segment</Table.Th>
+              <Table.Th className={styles.header}>Fans</Table.Th>
+              <Table.Th className={styles.header}>Ctr</Table.Th>
+              <Table.Th className={`${styles.header} ${styles.cellRight}`}>
+                Avg. Revenue
+              </Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>{rows}</Table.Tbody>
+        </Table>
+      </div>
+    </div>
+  )
+}
+
+export default TableWidget
