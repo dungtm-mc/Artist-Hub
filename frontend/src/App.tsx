e02d29482bf88b8a39832effd291f4b7b6ce564f
dungtm-mc <dungtm@missingcorner.com>
2024-10-15 09:52:00 +0700

    fix: import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import '@mantine/core/styles.css'
 
-import Logo from './components/Logo/logo'
+import Logo from './components/Logo'
 
 export default function App() {
   return <Logo mode="light" />
