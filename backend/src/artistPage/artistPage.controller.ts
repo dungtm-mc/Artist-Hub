414e281eb5e258198c4dd6949d1682f51f914613
bytesculptors <tuannghia200603@gmail.com>
2024-10-16 15:48:12 +0700

    feat: return left, right and center for page

--- backend/src/artistPage/artistPage.controller.ts
+++ backend/src/artistPage/artistPage.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
+import { ArtistPageService } from './artistPage.service'
 
 @Controller('artistPage')
-export class ArtistPageController {}
+export class ArtistPageController {
+  constructor(private artistPageService: ArtistPageService) {}
+  @Get('left')
+  async getLeft() {
+    return this.artistPageService.getLeftColumn()
+  }
+
+  @Get('right')
+  async getRight() {
+    return this.artistPageService.getRightColumn()
+  }
+
+  @Get('center')
+  async getCenter() {}
+}
